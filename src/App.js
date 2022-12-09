3883302134b3b729bb219d57c8e147f4c100db1b
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-12-09 11:48:29 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Todo from "./components/Todo";
+import Backdrop from "./UI/Backdrop";
+import Modal from "./UI/Modal";
 
 const App = () => {
   return (
@@ -6,6 +8,8 @@ const App = () => {
       <h1>My Todos</h1>
       <Todo title="Learn React" />
       <Todo title="Master React" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 };
